c4764e5a5f13a22a8ede39d64846a1cd3f0f3ad4
Tappy-TechX <tappyblack2@gmail.com>
2025-06-23 18:14:53 +0300

    𝐁ᴜᴍʙʟᴇʙᴇᴇ-𝐗ᴍᎠ

---
+++ config.js
@@ -0,0 +1,8 @@
+// USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
+module.exports = {
+  PREFIX: process.env.PREFIX || "Black-Tappy×",
+  EMAIL: process.env.EMAIL || "blacktappy5@gmail.com",
+  PASS: process.env.PASS || "blacktappytech",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "https://files.catbox.moe/8k0enh.jpg",
+}
